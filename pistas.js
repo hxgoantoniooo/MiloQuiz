7255ea5dfafc7434e1bd9015d30ae7593141d49c
hxgoantonio <163082826+hxgoantonio@users.noreply.github.com>
2026-03-08 23:40:10 -0300

    Version 1

---
+++ pistas.js
@@ -0,0 +1,17 @@
+const datosDiarios = [
+  {
+    "id": 1,
+    "respuestasHash": ["c29saWZpY2FuMTI=", "c29saWZpY2FuZG9zZQ=="],
+    "pistasTextos": [
+      "📅 Año de lanzamiento: 2025",
+      "💿 Pertenece a un álbum",
+      "🎤 No contiene feats",
+      "🎹 Fue cantada en el Festival de Viña"
+    ],
+    "audioUrl": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview211/v4/23/56/d5/2356d50b-385a-822d-0d31-39150645e87b/mzaf_12221268799444253918.plus.aac.p.m4a",
+    "audioInicio": 14,
+    "portada": "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f5/a6/33/f5a63325-8bd8-7e36-8850-878e2522169e/196873615486.jpg/300x300bb.jpg",
+    "tituloMostrar": "Solifican12",
+    "artistaMostrar": "Milo j"
+  }
+];
