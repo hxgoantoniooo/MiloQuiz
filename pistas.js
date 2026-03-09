4d41b9cd08ee3159efaa911e5c50757fea67f45e
hxgoantonio <163082826+hxgoantonio@users.noreply.github.com>
2026-03-09 20:16:31 -0300

    Version 2 (dia 2)

--- pistas.js
+++ pistas.js
@@ -13,5 +13,20 @@ const datosDiarios = [
     "portada": "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f5/a6/33/f5a63325-8bd8-7e36-8850-878e2522169e/196873615486.jpg/300x300bb.jpg",
     "tituloMostrar": "Solifican12",
     "artistaMostrar": "Milo j"
+  },
+  {
+    "id": 2,
+    "respuestasHash": ["dG9paGVjaG8="],
+    "pistasTextos": [
+      "📅 Año de lanzamiento: 2023",
+      "💿 Pertenece a un EP",
+      "🎤 No contiene feats",
+      "🎶 Parte de la letra: ''Soy el número uno sin que mi país lo sepa''"
+    ],
+    "audioUrl": "https://cdnt-preview.dzcdn.net/api/1/1/a/8/8/0/a8861193de8f77f24e8acdcca17728a3.mp3?hdnea=exp=1773097515~acl=/api/1/1/a/8/8/0/a8861193de8f77f24e8acdcca17728a3.mp3*~data=user_id=0,application_id=42~hmac=d684a827344616fc9cf4355e6100260cbf0ffdad67b724b6dd39846b105ebbfb",
+    "audioInicio": 16,
+    "portada": "https://cdn-images.dzcdn.net/images/cover/b2b05ff496d790a0ef6a9f8c1a3c822e/500x500-000000-80-0-0.jpg",
+    "tituloMostrar": "TOI HECHO",
+    "artistaMostrar": "Milo j"
   }
 ];
